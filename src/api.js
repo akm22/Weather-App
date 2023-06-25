122e6d84fe19c35849260f1f7c49b6f1a3943312
Arya <akmehta2@ncsu.edu>
2023-06-26 00:28:30 +0530

    final push

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c2127635eemsh916fac7f0f237eap1e40fejsn796270665468',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "269598f3e2a479935969cd41104fb870"; 
